788747aeb8e306fe6b77c5f483ed5b3447859c92
MarcosEdington <115179086+MarcosEdington@users.noreply.github.com>
2026-02-12 00:23:50 -0300

    Update Pessoa.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -13,7 +13,7 @@ namespace ControleGastosFamiliaApi.Models
         [Range(0, 120, ErrorMessage = "Idade deve ser entre 0 e 120 anos")]
         public int Idade { get; set; }
 
-        // Relação: uma pessoa pode ter várias transações (opcional manter, mas não obrigatório para JSON)
+        
         public List<Transacao> Transacoes { get; set; } = new();
     }
 }
